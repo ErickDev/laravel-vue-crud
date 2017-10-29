a02bece152e1f6da4c1a0b298e92b2562cf650b5
ErickDev <hlerick95@gmail.com>
2017-10-29 13:02:04 -0500

    Notificaciones con TOASTR

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -13,11 +13,15 @@ new Vue({
                 this.keeps = response.data
             });
         },
-        deleteKeep: function (keep) {
-            var urlKeep = 'tasks/' + keep.id;
-            axios.delete(urlKeep).then(response => {
-                this.getKeeps();
+        deleteKeep: function(keep) {
+            if (!confirm("¿Está seguro de eliminar?")) {
+                return false;
+            }
+            var url = 'tasks/' + keep.id;
+            axios.delete(url).then(response => { //eliminamos
+                this.getKeeps(); //listamos
+            toastr.success('Eliminado correctamente'); //mensaje
             });
-        }
+        }﻿
     }
 });
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,8 +9,7 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     <!-- Bootstrap -->
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
 <div class="container">
